a257439c7e2d63cc7ec57ad5d74612d3c31d8384
Thomas Wissemann <tcwissemann@gmail.com>
2017-01-23 20:15:26 -0600

    Create msg.py

---
+++ msg.py
@@ -0,0 +1,6 @@
+from twilio.rest import TwilioRestClient
+
+client = TwilioRestClient("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx") 
+
+client.messages.create(to="+1xxxxxxxxxx", from_="+1xxxxxxxxxx", body="Hello World!", media_url=["https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQ6eclMOzgr8WmjwRMQnGFLJdSKg7oM0gMRXaOppFkVQkL22ytM"])
+
